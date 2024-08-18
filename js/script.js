8846d3b31f36a418d43f91ca4cfbb76d8f1c9abb
Istiaqe Tanim <165544858+IstiaqeTanim@users.noreply.github.com>
2024-08-18 19:47:19 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+﻿/* ex1*/
+function showText(){
+	document.getElementById('s_text').innerHTML="Hello SSB";
+}
+/* ex2*/
+function showDate(){
+	document.getElementById('s_date').innerHTML=Date();
+}
+/* ex3*/
+function showState(){
+	document.getElementById('s_state').innerHTML="Hello Javascript!";
+}
+/* ex4*/
+function bulbOn(){
+	document.getElementById('img').src="image/on.gif";
+}
+function bulbOff(){
+	document.getElementById('img').src="image/off.gif";
+}
+/* ex5*/
+function styleChange(){
+	document.getElementById('css').style.color="red";
+}
+/* ex6*/
+function showContent(){
+	document.getElementById('con').style.display="block";
+}
+function hideContent(){
+	document.getElementById('con').style.display="none";
+}
+/* ex7*/
+let number1, number2;
+
+number1 = 5;
+number2 = 7;
+
+number1 += number2;
+function dataManipulate(){
+	document.getElementById('res').innerHTML=number1;
+}
+/* ex8*/
+var data = "Welcome to SS"
+function dataRepeat(){
+	document.getElementById('rep').innerHTML=data.repeat(3);
+}
+/* ex9*/
+let number3, number4, value;
+
+number3 = 10;
+number4 = 10;
+
+value = number3 == number4;
+function dataValue(){
+	document.getElementById('result').innerHTML=value;
+}
